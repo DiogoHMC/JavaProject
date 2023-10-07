504dbd56554e93b1f8bcc845e5495a35e276e075
Diogo Henrique de Melo Correia <diogohenriquedemelo28@gmail.com>
2023-10-07 15:38:17 -0300

    CaixaDeBonusDAO

--- Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/dao/CaixaDeBonusDAO.java
+++ Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/dao/CaixaDeBonusDAO.java
@@ -1,5 +1,19 @@
 package br.gov.cesarschool.poo.bonusvendas.dao;
 
+
+import br.gov.cesarschool.poo.bonusvendas.entidade.*;
+import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+
 public class CaixaDeBonusDAO {
-    
+    private static final long BRANCO= 0;
+    private CadastroObjetos cadastro = new CadastroObjetos(CaixaDeBonus.class);
+    		public boolean incluir(CaixaDeBonus prod) {
+		Produto prodBusca = buscar(prod.getNumeros()); 
+		if (prodBusca != null) { 
+			return false;
+		} else {
+			cadastro.incluir(prod, BRANCO + prod.getCodigo());
+			return true;
+		}		 
+	}
 }
