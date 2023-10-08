e2b20a35c300d8dd31ada1cf6af9230a418a6756
Matheus Gomes de Andrade <mga@cesar.school>
2023-10-08 19:57:53 -0300

    Criação de uma tela de cadastro (Incompleta)

--- Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/dao/CaixaDeBonusDAO.java
+++ Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/dao/CaixaDeBonusDAO.java
@@ -8,41 +8,59 @@ import java.io.Serializable;
 import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 
 public class CaixaDeBonusDAO {
+	
     private static final String BRANCO = "";
     private CadastroObjetos cadastro = new CadastroObjetos(CaixaDeBonus.class);
-		public boolean incluir(CaixaDeBonus prod) {
+    
+	public boolean incluir(CaixaDeBonus prod) {
+		
+		CaixaDeBonus prodBusca = buscar(prod.getNumeros()); 
+		
+		if (prodBusca != null) {
+				
+			return false;
+		} 
+		
+		else {
 			
-			CaixaDeBonus prodBusca = buscar(prod.getNumeros()); 
-			if (prodBusca != null) { 
-				return false;
-			} else {
-				cadastro.incluir(prod, BRANCO + prod.getNumeros());
-				return true;
-			}		 
-		}
-		public boolean alterar(CaixaDeBonus prod) {
+			cadastro.incluir(prod, BRANCO + prod.getNumeros());
+			return true;
+		}		 
+	}
+	
+	public boolean alterar(CaixaDeBonus prod) {
 			
-			CaixaDeBonus prodBusca = buscar(prod.getNumeros());
-			if (prodBusca == null) {
-				return false;
-			} else {
-				cadastro.alterar(prod, BRANCO + prod.getNumeros());
-				return true;
-			}		
-		}
-		public CaixaDeBonus buscar(long numeros) {
-			// Esta operação entre () vai ter significado mais à frente!
-			return (CaixaDeBonus)cadastro.buscar(BRANCO + numeros);
+		CaixaDeBonus prodBusca = buscar(prod.getNumeros());
+		if (prodBusca == null) {
+			
+			return false;
+		} 
+		
+		else {
+			
+			cadastro.alterar(prod, BRANCO + prod.getNumeros());
+			return true;
+		}		
+	}
+
+	public CaixaDeBonus buscar(long numeros) {
+		
+		// Esta operação entre () vai ter significado mais à frente!
+		return (CaixaDeBonus)cadastro.buscar(BRANCO + numeros);
+	}
+	
+	public CaixaDeBonus[] buscarTodos() {
+		
+		Serializable[] rets = cadastro.buscarTodos(CaixaDeBonus.class);
+		CaixaDeBonus[] prods = new CaixaDeBonus[rets.length];
+		
+		for(int i=0; i<rets.length; i++) {
+			
+			// Esta operação entre () vai ter significado mais à frente! 
+			prods[i] = (CaixaDeBonus)rets[i];
 		}
 		
-		public CaixaDeBonus[] buscarTodos() {
-			Serializable[] rets = cadastro.buscarTodos(CaixaDeBonus.class);
-			CaixaDeBonus[] prods = new CaixaDeBonus[rets.length];
-			for(int i=0; i<rets.length; i++) {
-				// Esta operação entre () vai ter significado mais à frente! 
-				prods[i] = (CaixaDeBonus)rets[i];
-			}
-			return prods;
-		} 
+		return prods;
+	} 
 
 }
--- Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/dao/LancamentoBonusDAO.java
+++ Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/dao/LancamentoBonusDAO.java
@@ -6,39 +6,59 @@ import java.io.Serializable;
 import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 
 public class LancamentoBonusDAO {
+	
 	private static final String BRANCO = "";
 	private CadastroObjetos cadastro = new CadastroObjetos(LancamentoBonus.class); 
+	
 	public boolean incluir(LancamentoBonus prod) {
+		
 		long timestamp = prod.getDataHoraLancamento().toEpochSecond(java.time.ZoneOffset.UTC);
 		LancamentoBonus prodBusca = buscar(timestamp); 
+		
 		if (prodBusca != null) { 
+			
 			return false;
-		} else {
+		} 
+		
+		else {
 			cadastro.incluir(prod, BRANCO + timestamp);
 			return true;
 		}		 
 	}
+	
 	public boolean alterar(LancamentoBonus prod) {
+		
 		long timestamp = prod.getDataHoraLancamento().toEpochSecond(java.time.ZoneOffset.UTC);
 		LancamentoBonus prodBusca = buscar(timestamp);
+		
 		if (prodBusca == null) {
+			
 			return false;
-		} else {
+		} 
+		
+		else {
+			
 			cadastro.alterar(prod, BRANCO + timestamp);
 			return true;
 		}		
 	}
 	public LancamentoBonus buscar(long timestamp) {
+		
 		// Esta operação entre () vai ter significado mais à frente! 
 		return (LancamentoBonus)cadastro.buscar(BRANCO + timestamp);
 	}
+	
 	public LancamentoBonus[] buscarTodos() {
+		
 		Serializable[] rets = cadastro.buscarTodos(LancamentoBonus.class);
 		LancamentoBonus[] prods = new LancamentoBonus[rets.length];
+		
 		for(int i=0; i<rets.length; i++) {
+			
 			// Esta operação entre () vai ter significado mais à frente! 
 			prods[i] = (LancamentoBonus)rets[i];
 		}
+		
 		return prods;
 	}    
 }
