099615b9c0fe7237647ec3d1b78b2c8874946237
SofiaValadares <svc3@cesar.school>
2023-10-08 17:32:29 -0300

    ResultadoInclusaoVendedor e parte AcumuloResgateMediator, + .jar

--- Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/negocio/AcumuloResgateMediator.java
+++ Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/negocio/AcumuloResgateMediator.java
@@ -1,5 +1,33 @@
 package br.gov.cesarschool.poo.bonusvendas.negocio;
 
+import java.time.LocalDate;
+
+import br.gov.cesarschool.poo.bonusvendas.dao.*;
+import br.gov.cesarschool.poo.bonusvendas.entidade.*;
+
 public class AcumuloResgateMediator {
+    private CaixaDeBonusDAO repositorioCaixaDeBonus;
+    private LancamentoBonusDAO repositorioLancamento;
     
+    public AcumuloResgateMediator() {
+    	this.repositorioCaixaDeBonus = new CaixaDeBonusDAO();
+    	this.repositorioLancamento = new LancamentoBonusDAO();
+    }
+    
+    public long gerarCaixaDeBonus(Vendedor vend) {
+    	String cpf = vend.getCpf();
+    	
+    	
+    	LocalDate dataAtual = LocalDate.now();
+    	
+    	String anoAtual = String.format("%0" + 4 + "%d", dataAtual.getYear());
+    	String mesAtual = String.format("%0" + 2 + "%d", dataAtual.getMonthValue());
+    	String diaAtual = String.format("%0" + 2 + "%d", dataAtual.getDayOfMonth());
+    	
+    	Long numero = Long.parseLong(cpf + anoAtual + mesAtual + diaAtual);
+
+        
+    	
+    	return 00000;
+    }
 }
--- Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/negocio/ResultadoInclusaoVendedor.java
+++ Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/negocio/ResultadoInclusaoVendedor.java
@@ -1,5 +1,21 @@
 package br.gov.cesarschool.poo.bonusvendas.negocio;
 
 public class ResultadoInclusaoVendedor {
+    private long numeroCaixaDeBonus;
+    private String mensagemErroValidacao;
+    
+    public ResultadoInclusaoVendedor(long caixa, String mensagem) {
+    	this.numeroCaixaDeBonus = caixa;
+    	this.mensagemErroValidacao = mensagem;
+    }
+    
+    
+    public long getNumeroCaixaDeBonus() {
+    	return this.numeroCaixaDeBonus;
+    }
+    
+    public String getMensagemErroValidacao() {
+    	return this.mensagemErroValidacao;
+    }
     
 }
