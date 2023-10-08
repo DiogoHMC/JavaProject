0ff6dde32bd35e1b0185176780e6dd90e174c339
SofiaValadares <113111708+SofiaValadares@users.noreply.github.com>
2023-10-08 16:37:13 -0300

    StringUtil

--- Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/StringUtil.java
+++ Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/StringUtil.java
@@ -2,4 +2,15 @@ package br.gov.cesarschool.poo.bonusvendas.negocio.geral;
 
 public class StringUtil {
     
-}
+	private StringUtil() {
+		
+	}
+	
+	public static boolean ehNuloOuBranco(String str) {
+		
+		String minhaString = str.replaceAll("\\s+", "");
+		
+		return minhaString == null || minhaString.equals("");
+		
+	}
+
