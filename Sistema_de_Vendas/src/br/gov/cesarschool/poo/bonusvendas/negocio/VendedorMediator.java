d2aed44a2a2f6cb4a9b6767ecba3f4715a595f6f
Yara <yara.rodrigues.inacio@gmail.com>
2023-10-09 08:05:08 -0300

    StringUtil e ValidadorCPF

--- Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/negocio/VendedorMediator.java
+++ Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/negocio/VendedorMediator.java
@@ -1,5 +1,5 @@
 package br.gov.cesarschool.poo.bonusvendas.negocio;
 
 public class VendedorMediator {
-    
+	
 }
--- Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/StringUtil.java
+++ Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/StringUtil.java
@@ -1,5 +1,11 @@
 package br.gov.cesarschool.poo.bonusvendas.negocio.geral;
 
 public class StringUtil {
-    
+    private StringUtil() {
+        // Construtor privado para evitar instância da classe
+    }
+
+    public static boolean ehNuloOuBranco(String str) {
+        return str == null || str.trim().isEmpty();
+    }
 }
