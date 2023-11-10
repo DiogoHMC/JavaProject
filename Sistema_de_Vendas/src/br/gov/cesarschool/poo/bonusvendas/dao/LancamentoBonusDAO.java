bc78e9d26d7903480e86900ffe23b5e51fd59fab
SofiaValadares <113111708+SofiaValadares@users.noreply.github.com>
2023-11-10 09:07:08 -0300

    Update LancamentoBonusDAO.java

--- Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/dao/LancamentoBonusDAO.java
+++ Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/dao/LancamentoBonusDAO.java
@@ -1,9 +1,5 @@
 package br.gov.cesarschool.poo.bonusvendas.dao;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import br.gov.cesarschool.poo.bonusvendas.entidade.LancamentoBonus;
 
 public class LancamentoBonusDAO {
@@ -14,37 +10,30 @@ public class LancamentoBonusDAO {
     }
 	
 	public boolean incluir(LancamentoBonus lancamento) {
-		String idUnico = getIdUnico(lancamento);
+		String idUnico = lancamento.getIdUnico();
 		LancamentoBonus lancamentoBusca = buscar(idUnico);  
 		if (lancamentoBusca != null) { 
 			return false;
 		} else {
-			cadastro.incluir(lancamento, idUnico);
+			dao.incluir(lancamento);
 			return true;
 		}		 
 	}
 	public boolean alterar(LancamentoBonus lancamento) {
-		String idUnico = obterIdUnico(lancamento);
+		String idUnico = lancamento.getIdUnico();
 		LancamentoBonus lancamentoBusca = buscar(idUnico);
 		if (lancamentoBusca == null) {
 			return false;
 		} else {
-			cadastro.alterar(lancamento, idUnico);
+			dao.alterar(lancamento);
 			return true;
 		}		
 	}
 	public LancamentoBonus buscar(String codigo) {
-		// Esta operacao entre () vai ter significado mais a frente! 
-		return (LancamentoBonus)cadastro.buscar(codigo);
+		return dao.buscar(codigo);
 	}
 	public LancamentoBonus[] buscarTodos() {
-		Serializable[] rets = cadastro.buscarTodos(LancamentoBonus.class);
-		LancamentoBonus[] lancamentos = new LancamentoBonus[rets.length];
-		for(int i=0; i<rets.length; i++) {
-			// Esta operacao entre () vai ter significado mais a frente! 
-			lancamentos[i] = (LancamentoBonus)rets[i];
-		}
-		return lancamentos;
+		return dao.buscarTodos();
 	} 
 
 }
