96dbb0bd025c7e6445c213de43b253b28a508e1c
Yara-R <yara.rodrigues.inacio@gmail.com>
2023-11-24 09:19:46 -0300

    ExecaoObjetoNaoExistente e DAOFenerico

--- Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/excecoes/ExcecaoObjetoNaoExistente.java
+++ Sistema_de_Vendas/src/br/gov/cesarschool/poo/bonusvendas/excecoes/ExcecaoObjetoNaoExistente.java
@@ -1,5 +1,12 @@
 package br.gov.cesarschool.poo.bonusvendas.excecoes;
 
-public class ExcecaoObjetoNaoExistente {
+public class ExcecaoObjetoNaoExistente extends Exception {
 
+	public ExcecaoObjetoNaoExistente() {
+        super("Objeto nao existente");
+    }
+
+    public ExcecaoObjetoNaoExistente(String mensagem) {
+        super(mensagem);
+    }
 }
